0f254cab298aac1eab5ded785948f26f74d099a5
Osi <osachi752@gmail.com>
2025-05-15 15:37:59 -0400

    basic server setup

---
+++ server.mjs
@@ -0,0 +1,46 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+// Route Modules
+import characterRoutes from './routes/characters.mjs';
+import songRoutes from './routes/songs.mjs';
+import locationRoutes from './routes/locations.mjs';
+
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors()); // Enable CORS for all routes
+
+// Connect to MongoDB using Mongoose
+mongoose.connect(process.env.ATLAS_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
+// API Routes - Mount the route handlers
+app.use('/api/characters', characterRoutes);
+app.use('/api/songs', songRoutes);
+app.use('/api/locations', locationRoutes);
+// app.use('/api/auth', authRoutes); // Authentication routes
+
+// Base Route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Sound of Music API!');
+});
+
+// Global error handling
+app.use((err, _req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Seems like we messed up somewhere...");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
