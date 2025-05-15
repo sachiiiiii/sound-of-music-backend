5b01a02c8dc26dc5bdd16bf3e5c23da11c8511e6
Osi <osachi752@gmail.com>
2025-05-15 17:33:10 -0400

    added seed route

--- routes/locations.mjs
+++ routes/locations.mjs
@@ -1,4 +1,5 @@
 import express from 'express';
+import Location from '../models/location.mjs';
 // Controller functions
 import {
   getAllLocations,
@@ -6,15 +7,29 @@ import {
   createLocation,
   updateLocation,
   deleteLocation
-} from '../controllers/locations.controller.js';
+} from '../controllers/locations.controller.mjs';
 
 const router = express.Router();
+
+router.get('/seed', async (req, res) => {
+    try {
+        await Location.create([
+            { name: 'Nonnberg Abbey', city: 'Salzburg', country: 'Austria', description: 'Maria\'s convent' },
+            { name: 'Von Trapp Villa', city: 'Salzburg', country: 'Austria', description: 'The family home' },
+            { name: 'Mirabell Gardens', city: 'Salzburg', country: 'Austria', description: 'Iconic filming location' },
+            { name: 'Salzburg Residenz Fountain', city: 'Salzburg', country: 'Austria', description: 'Featured in Do-Re-Mi' }
+        ])
+        res.redirect('/api/locations');
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // Define '/api/locations' endpoints and link them to controller functions
 router.get('/', getAllLocations);
 router.get('/:id', getLocationById);
 //router.post('/', createLocation);
-router.put('/:id', updateLocation); // PUT instead PATCH for full updates
+router.put('/:id', updateLocation); // PUT instead of PATCH for full updates
 //router.delete('/:id', deleteLocation);
 
 export default router;
